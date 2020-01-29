51cbda4a5769404a91933c35c6de8531aa9ffd50
HitaloKun <hitalomarquete331@gmail.com>
2020-01-29 20:43:25 +0000

    [biography] fixes

--- hitsuki/modules/biography.py
+++ hitsuki/modules/biography.py
@@ -51,6 +51,7 @@ def set_bio(bot: Bot, update: Update):
 
 
 def __user_info__(user_id, chat_id):
+	chat = update.effective_chat  # type: Optional[Chat]
     bio = html.escape(sql.get_user_bio(user_id) or "")
     if bio:
         return tld(chat.id, "<b>About user:</b>\n{bio}\n".format(bio=bio))
