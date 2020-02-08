c080223ca69a8b64d216a2ed9b6a284393527163
HitaloKun <hitalomarquete331@gmail.com>
2020-02-08 10:47:27 -0300

    [xiaomi specs] last test

--- hitsuki/modules/xiaomi_specs.py
+++ hitsuki/modules/xiaomi_specs.py
@@ -13,7 +13,7 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 @run_async
 def xspecs(bot, update, args):
     device = " ".join(args)
-    message = ''
+    message = xspecs(device)
     if len(args) == 0:
         reply = f'No codename provided, write a codename for fetching informations.'
         update.effective_message.reply_text("{}".format(reply),
