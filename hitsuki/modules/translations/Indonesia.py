bd8aa71449d3adb4617d2c67dd8a63c484ca1d2d
MhdRezza <67638930+MhdRezza@users.noreply.github.com>
2020-07-01 06:55:37 +0700

    Update and rename Indonesia to Indonesia.py
    Add indo

---
+++ hitsuki/modules/translations/Indonesia.py
@@ -0,0 +1,231 @@
+
+RUN_STRINGS = (
+"Di mana Anda pikir Anda akan pergi? ", 
+"Hah? Apa? Apakah mereka lolos?",
+"ZZzzZZzz ... Hah? Apa? Oh, hanya mereka lagi, nevermind",
+"Kembali kesini!",
+"Tidak terlalu cepat ...",
+"Carilah dinding!",
+"Jangan tinggalkan aku sendiri dengan mereka !!", 
+"Kamu lari, kamu mati.", 
+"Lelucon padamu, aku di mana-mana",
+"Kau akan menyesali itu ...", 
+"Kamu juga bisa mencoba / menendang, aku dengar itu menyenangkan.", 
+"Pergi mengganggu orang lain, tidak ada yang peduli.", 
+"Kamu bisa lari, tetapi kamu tidak bisa sembunyi.", 
+"Hanya itu yang kamu punya?", 
+"Saya di belakang Anda...", 
+"Kamu punya teman!",
+"Kita bisa melakukan ini dengan cara yang mudah, atau dengan cara yang sulit.",
+"Kamu tidak mengerti, kan?",
+"Ya, kamu sebaiknya lari!", 
+"Tolong, ingatkan aku betapa aku peduli?", 
+"Aku akan berlari lebih cepat jika aku jadi kamu.", 
+"Itu pasti droid yang kita cari.", 
+"Semoga peluangnya menguntungkan Anda.",
+"Kata-kata terakhir yang terkenal.",
+"Dan mereka menghilang selamanya, tidak pernah terlihat lagi.",
+    "\" Oh, lihat aku! Saya sangat keren, saya bisa lari dari bot! \ "- orang ini", "Yeah yeah, cukup ketuk / tendang saja.", "Ini, ambil cincin ini dan pergi ke Mordor saat kamu sedang di sana.", "Legenda memilikinya, mereka masih berlari ...", "Tidak seperti Harry Potter, orang tuamu tidak bisa melindungimu dariku.", "Ketakutan mengarah pada kemarahan. Kemarahan mengarah pada kebencian. Kebencian mengarah pada penderitaan. Jika kamu terus berlari dalam ketakutan, kamu mungkin" "jadilah Vader berikutnya.", "Beberapa perhitungan kemudian, aku telah memutuskan bahwa ketertarikanku pada kejahatanmu adalah tepat 0.", "Legenda memilikinya, mereka masih berlari.", "Terus, tidak yakin kami mau kamu di sini.", "Kamu penyihir- Oh. Tunggu. Kamu bukan Harry, terus bergerak.", "TIDAK BERJALAN DI HALLWAYS!", "Hasta la vista, sayang.", "Siapa yang membiarkan anjing keluar?", "Lucu, karena tidak ada yang peduli.", "Ah, sayang sekali. Aku suka yang itu.", "Terus terang, sayangku, aku tidak peduli.", "Milkshake-ku membawa semua anak laki-laki ke halaman ... Jadi cepatlah lari!", "Kamu tidak bisa MENANGANI kebenaran!", "Dahulu kala, di galaksi yang sangat jauh ... Seseorang pasti akan peduli tentang itu. Tidak lagi.", "Hei, lihat mereka! Mereka lari dari penghempas yang tak terhindarkan ... Manis.", "Han menembak lebih dulu. Aku juga.", "Apa yang kamu kejar, kelinci putih?", "Seperti kata Dokter ... LARI!",
+)
+
+INSULTS_STRINGS = (
+    "Owww ... Bodoh sekali.", "Jangan minum dan mengetik.", "Aku pikir kamu harus pulang atau lebih baik rumah sakit jiwa.",
+   "Perintah tidak ditemukan. Sama seperti otakmu.", 
+"Apakah kamu sadar kamu membodohi dirimu sendiri? Rupanya tidak.",
+"Anda bisa mengetik lebih baik dari itu.", 
+"Peraturan Bot 544 bagian 9 mencegahku membalas ke orang bodoh sepertimu.",
+"Maaf, kami tidak menjual otak.",
+ "Percayalah, kamu tidak normal.",
+"Aku yakin otakmu terasa seperti baru, karena kamu tidak pernah menggunakannya.",
+ "Jika aku ingin bunuh diri aku akan memanjat egomu dan melompat ke IQ mu.",
+"Zombi memakan otak ... kamu aman.", 
+"Kamu tidak berevolusi dari kera, mereka berevolusi dari kamu.",
+"Kembalilah dan berbicaralah kepadaku ketika AKU kamu melebihi usiamu.", 
+"Aku tidak mengatakan kamu bodoh, aku hanya mengatakan kamu bernasib buruk ketika harus berpikir.", 
+"Bahasa apa yang kamu bicarakan? Karena kedengarannya seperti omong kosong.",
+"Kebodohan bukanlah kejahatan sehingga kamu bebas untuk pergi.",
+"Kamu adalah bukti bahwa evolusi BISA terbalik.",
+"Aku akan bertanya berapa umurmu, tapi aku tahu kamu tidak bisa menghitung setinggi itu.",
+"Sebagai orang luar, apa pendapatmu tentang ras manusia?",
+"Otak bukan segalanya. Dalam kasusmu itu bukan apa-apa.",
+"Biasanya orang hidup dan belajar. Kamu hanya hidup.", 
+"Aku tidak tahu apa yang membuatmu begitu bodoh, tetapi itu benar-benar berhasil.",
+   "Teruslah bicara, suatu hari kamu akan mengatakan sesuatu yang cerdas! (Aku ragu itu)",
+"Kejutkan aku, katakan sesuatu yang cerdas.",
+"IQ kamu lebih rendah dari ukuran sepatu kamu.",
+ "Aduh! Neurotransmiter Anda tidak lagi berfungsi.", "Apakah kamu gila, kamu bodoh.",
+ "Setiap orang memiliki hak untuk menjadi bodoh tetapi Anda menyalahgunakan hak istimewa.",
+ "Maaf aku menyakiti perasaanmu ketika aku menyebutmu bodoh. Aku pikir kamu sudah tahu itu.", 
+"Kamu harus mencoba mencicipi sianida.", 
+"Enzimmu dimaksudkan untuk mencerna racun tikus.", 
+"Kamu harus mencoba tidur selamanya.", 
+"Ambil pistol dan tembak dirimu sendiri.", 
+"Kamu bisa membuat rekor dunia dengan melompat dari pesawat tanpa parasut.",
+ "Berhenti bicara BS dan lompat ke depan kereta peluru.", 
+"Cobalah mandi dengan Hydrochloric Acid daripada air.", 
+"Coba ini: jika kamu menahan nafas di bawah air selama satu jam, maka kamu dapat menahannya selamanya.",
+ "Go Green! Berhenti menghirup Oksigen.", 
+"Tuhan sedang mencarimu. Kau harus pergi untuk menemuinya.",
+ "Berikan 100% milikmu. Sekarang, lakukan donor darah.", 
+"Coba lompat dari gedung bertingkat seratus tetapi kamu hanya bisa melakukannya sekali.", 
+"Kamu harus menyumbangkan otakmu melihat bahwa kamu tidak pernah menggunakannya.",
+ "Relawan untuk target dalam jarak tembak.",
+ "Tembakan kepala itu menyenangkan. Dapatkan satu untukmu sendiri.",
+ "Kamu harus mencoba berenang dengan hiu putih besar.", 
+"Kamu harus mengecat dirimu sendiri merah dan berlari dalam maraton banteng.",
+ "Kamu bisa tetap di bawah air selama sisa hidupmu tanpa naik kembali.",
+    "Bagaimana kalau kamu berhenti bernapas selama 1 hari? Itu akan bagus.", 
+"Cobalah memprovokasi harimau saat kalian berdua berada di kandang.", 
+"Sudahkah kamu mencoba memotret diri sendiri setinggi 100m menggunakan kamera.",
+ "Anda harus mencoba memegang Mercon di mulut Anda dan menyalakannya.",
+ "Coba mainkan tangkapan dan lempar dengan RDX itu menyenangkan.",
+"Aku dengar kopi beracun tapi kurasa kamu tidak keberatan meminumnya untuk bersenang-senang.", 
+"Luncurkan dirimu ke luar angkasa sambil melupakan oksigen di Bumi.", 
+"Kamu harus mencoba bermain ular dan tangga, dengan ular sungguhan dan tanpa tangga.", 
+"Menari telanjang di beberapa kabel HT.",
+ "True Volcano adalah kolam renang terbaik untukmu.", 
+"Kamu harus mencoba mandi air panas di gunung berapi.", 
+"Cobalah menghabiskan satu hari di peti mati dan itu akan menjadi milikmu selamanya.",
+ "Pukul Uranium dengan neutron yang bergerak lambat di hadapanmu. Itu akan menjadi pengalaman yang berharga.", 
+"Kamu bisa menjadi orang pertama yang menginjak matahari. Selamat mencoba.",
+)
+
+SLAP_TEMPLATES = (
+    "{user1} {hits} {user2} dengan {item}.",
+"{user1} {hits} {user2} di wajah dengan {item}.",
+"{user1} {hits} {user2} sekitar sedikit dengan {item}.",
+"{user1} {throws} a {item} di {user2}.",
+"{user1} meraih {item} dan {throws} di muka {user2}.",
+    "{user1} meluncurkan {item} ke perut {user2}.",
+    "{user1} mulai menampar {user2} konyol dengan {item}.",
+ "{user1} pin {user2} turun dan berulang kali {hits} mereka dengan {item}.", 
+"{user1} meraih {item} dan {hits} {user2} dengan itu.", 
+"{user1} mengikat {user2} ke kursi dan {melempar} sebuah {item} pada mereka.", 
+"{user1} memberi dorongan mesra untuk membantu {user2} belajar berenang di lava."
+)
+
+ITEMS = (
+    "wajan besi cor",
+    "trout besar",
+    "tongkat pemukul baseball",
+    "Kelelawar kriket",
+    "tongkat kayu",
+    "kuku",
+    "pencetak",
+    "sekop",
+    "Monitor CRT",
+    "buku teks fisika",
+    "pemanggang roti",
+    "potret Richard Stallman",
+    "televisi",
+    "truk lima ton",
+    "Gulungan lakban",
+    "buku",
+    "laptop",
+    "televisi lama",
+    "Karung batu",
+    "rainbow trout",
+    "ayam karet",
+    "Kelelawar berduri",
+    "Pemadam Api",
+    "heavy rock",
+    "potongan tanah",
+    "sarang lebah",
+    "Sepotong daging busuk",
+    "beruang",
+    "Banyak bata",
+)
+
+THROW = (
+    "melempar",
+    "teman kencan",
+    "chuck",
+    "melemparkan",
+)
+
+HIT = (
+    "hit",
+    "pukulan keras",
+    "menampar",
+    "memukul",
+    "bash",
+)
+
+MARKDOWN_HELP = """
+Anda dapat menggunakan penurunan harga untuk membuat pesan Anda lebih ekspresif. Ini adalah penurunan harga yang saat ini didukung:
+
+<code> `code words` </code> backticks memungkinkan Anda untuk membungkus kata-kata Anda dalam font monospace. Ditampilkan sebagai: <code> kata-kata kode </code>
+<code> * kata-kata tebal * </code> asterix digunakan untuk huruf tebal. Ditampilkan sebagai: <b> kata-kata tebal </b>
+<code> _italic words_ </code> Garis bawah digunakan untuk huruf miring. Ditampilkan sebagai: <i> cetak miring. </i>
+<code> [hyperlink] (example.com) </code> Ini digunakan untuk <code> [hyperlink] (http://example.com/) </code>, dan itu akan ditampilkan seperti itu: hyperlink. Pastikan untuk tidak menambahkan spasi tambahan antara] dan (atau itu tidak akan menjadi pengurangan harga yang valid.
+
+Sekarang, jika Anda ingin memiliki tombol pada pesan Anda, Anda bisa menggunakan sintaks khusus ini:
+<code> [tombol] (buttonurl: //example.com) </code>
+Ini akan membuat tombol bernama "tombol" yang mengarahkan pengguna ke example.com setelah mengklik.
+Jika Anda ingin menambahkan dua tombol pada baris yang sama, tambahkan: sama di akhir tautan Anda; itu akan mengaturnya pada baris yang sama dengan yang lainnya. Sebagai contoh:
+<code> [tombol] (buttonurl: //example.com)
+[tombol 2] (buttonurl: //example.com: sama)
+[tombol 3] (buttonurl: //example.com) </code>
+akan membuat dua tombol pada baris yang sama (tombol 1 dan 2), dan yang terakhir (tombol 3) pada baris kedua.
+
+Fillings:
+Anda juga dapat menggunakan tag tertentu untuk mengisi pesan Anda dengan pengguna atau info obrolan; opsinya adalah:
+<code> {first} </code>: Nama depan pengguna.
+<code> {last} </code>: Nama belakang pengguna.
+<code> {fullname} </code>: Nama lengkap pengguna.
+<code> {username} </code>: Nama pengguna pengguna; jika tidak ada yang tersedia, sebutkan pengguna.
+<code> {mention} </code>: Menyebutkan pengguna, menggunakan nama depan mereka.
+<code> {id} </code>: Id pengguna.
+<code> {chatname} </code>: Nama obrolan.
+<code> {rules} </code>: Menambahkan tautan ke aturan obrolan.
+<code> {preview} </code>: Mengaktifkan pratinjau tautan untuk pesan ini. Dapat bermanfaat saat menggunakan tautan ke halaman Tampilan Instan.
+
+Contoh cara menggunakan tambalan adalah mengatur sambutan Anda, melalui:
+<code> / setwelcome Hai di sana {pertama}! Selamat datang di {chatname}. </code>
+
+Cobalah ini pada catatan, filter, pesan selamat datang atau bahkan aturan!
+"""
+
+EnglishStrings = {
+    "send-start": "" "Hai {}, nama saya {}! Jika Anda memiliki pertanyaan tentang cara menggunakan saya, baca / bantu - dan kemudian menuju ke @YanaBotGroup.
+
+Saya bot manajer grup yang dikelola oleh [orang ini] (tg: // user? Id = {654839744}). Saya seorang [Marie] (https://github.com/PaulSonOfLars/tgbot) fork.
+Saya dibangun di python3, menggunakan \
+pustaka python-telegram-bot, dan saya sepenuhnya opensource - Anda dapat menemukan apa yang membuat saya centang \
+[di sini] (https://github.com/peaktogoo/HarukaAya))!
+
+Jangan ragu untuk mengirimkan permintaan tarik di github, atau menghubungi grup dukungan saya, @EchelonTeam, dengan bug, pertanyaan \
+atau permintaan fitur yang mungkin Anda miliki :)
+
+Jika Anda menikmati menggunakan saya dan / atau ingin membantu saya bertahan di alam liar, tekan / donasi untuk membantu mendanai / meningkatkan VPS saya!
+"" ",
+
+    "send-help": "" "Hai! Nama saya * {} *. Saya adalah bot manajemen grup, di sini untuk membantu Anda berkeliling dan menjaga ketertiban dalam grup Anda!
+Saya memiliki banyak fitur praktis, seperti pengendalian banjir, sistem peringatan, sistem penyimpanan catatan, dan bahkan balasan yang telah ditentukan pada kata kunci tertentu.
+
+* Beberapa perintah praktis adalah: *
+ - / mulai: mulai aku! Anda mungkin sudah menggunakan ini.
+ - / help: mengeluarkan pesan ini; Saya akan bercerita lebih banyak tentang diri saya!
+ - / help <module name>: PM Anda info tentang modul itu.
+ - / lang: Ganti bahasa bot
+ - / sumber: Info tentang kode sumber saya
+ - / pengaturan:
+   - dalam grup: Akan mengarahkan Anda ke pm, dengan semua pengaturan obrolan itu.
+   {}
+   "" ",
+
+    "send-group-settings": """Hai, di sana! Ada beberapa pengaturan untuk * {} * - lanjutkan dan pilih apa
+Anda tertarik. """,
+
+#Misc
+"RUNS-K": RUN_STRINGS,
+"SLAP_TEMPLATES-K": SLAP_TEMPLATES,
+"ITEMS-K": ITEMS,
+"HIT-K": HIT,
+"THROW-K": THROW,
+"ITEMP-K": ITEMS,
+"ITEMR-K": ITEMS,
+"MARKDOWN_HELP-K": MARKDOWN_HELP,
+"INSULTS-K": INSULTS_STRINGS,
+
+}
